c26f58a85dc830ad5047e2172aa2cf63e9d00215
Ches Rowe <chesrowe@gmail.com>
2023-12-26 23:32:24 -0500

    Purge

--- Augury-Docs/Augury5/Struct-functions.md
+++
@@ -1,5 +0,0 @@
-# Struct functions
-Functions related to structs
-
-## `struct_copy(src,dest)` → *bool.true <span style="color: red;"> *or* </span> -1*
-Copy all struct variables and values to another
--- Augury-Docs/config.js
+++ Augury-Docs/config.js
@@ -1,7 +1,7 @@
 const config = {
      themeColor: "#FFFFFF",
-   latestVersion: "Augury7",
-   otherVersions: [ "Augury", "Augury2", "Augury3", "Augury4", "Augury5", "Augury6" ],
+   latestVersion: "Augury",
+   otherVersions: [ ],
    name: "Tome",
    description: "Tome is a documentation generator for GameMaker Studio 2.3. Its designed to be easy to use, and easy to integrate into your workflow. Its also open source, so you can contribute to it if you want to!",
    favicon: "assets/docsIcon.png" ,
