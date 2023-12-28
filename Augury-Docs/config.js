8e2b79aefad4205186e8c2cad44df26de4fd1818
Ches Rowe <chesrowe@gmail.com>
2023-12-27 21:56:50 -0500

    Update Augury-Docs/config.js

--- Augury-Docs/config.js
+++ Augury-Docs/config.js
@@ -1,8 +1,29 @@
+/*
+	UPDATING VERSIONS:
+	1. Just change the `latestVersion` variable to the new version.
+	2. Put the old version into the `otherVersions` array.
+	
+	PREVIEWING CHANGES:
+	-	If you'd like to make future modifications to markdown files and preview them before pushing your changes,
+		you can do so with either Python or Node. If you have Node installed you can install Docsify with
+		'npm i docsify-cli -g' and then navigate to the docs folder and run 'docsify serve'. With Python you
+		can manually serve your docs with 'cd docs && python -m http.server 3000'.
+		More info: https://docsify.js.org/#/quickstart
+	
+	OTHER THINGS OF NOTE:
+	-	If you want a different sidebar or navbar for different versions,
+		you can just copy and paste the _sidebar.md or _navbar.md from the
+		main /docs/ folder into the version folder you'd like and modify it there.
+	
+	-	This should really be the only file you'll need to mess with, if any other changes are needed
+		please let @FaultyFunctions know!
+*/
+
 const config = {
-     themeColor: "#DDDDFF",
-   latestVersion: "Augury",
-   otherVersions: [ "Augury", "Augury2", "Augury3", "Augury4", "Augury5", "Augury6", "Augury7" ],
-   name: "Tome",
-   description: "Tome is a documentation generator for GameMaker Studio 2.3. Its designed to be easy to use, and easy to integrate into your workflow. Its also open source, so you can contribute to it if you want to!",
-   favicon: "assets/docsIcon.png" ,
+	name: 'Augury',
+	description: 'Code documentation for Augury',
+	latestVersion: 'Augury',
+	otherVersions: [],
+	favicon: 'assets/docsIcon.png',
+	themeColor: '#dc8624',
 };
