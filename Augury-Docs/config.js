82503fa20e490f8410911a9dbaf613a9cc0205a0
Ches Rowe <chesrowe@gmail.com>
2024-01-04 13:57:24 -0500

    Update Augury-Docs/config.js

--- Augury-Docs/config.js
+++ Augury-Docs/config.js
@@ -1,8 +1,8 @@
 const config = {
-     favicon: "assets/docsIcon.png",
+     themeColor: "#DDDDFF",
    latestVersion: "Augury",
    otherVersions: [ "Augury", "Augury2", "Augury3", "Augury4", "Augury5", "Augury6", "Augury7" ],
    name: "Tome",
    description: "Tome is a documentation generator for GameMaker Studio 2.3. Its designed to be easy to use, and easy to integrate into your workflow. Its also open source, so you can contribute to it if you want to!",
-   themeColor: "#DDDDFF" ,
+   favicon: "assets/docsIcon.png" ,
 };
